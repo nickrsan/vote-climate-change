9ae1648013bbeba5d8782cf61a9ff6cc5b1c24ae
Nick Santos <nickrsan@users.noreply.github.com>
2012-10-28 11:34:53 -0700

    Fix to restart management command

--- voteclimate/management/commands/restart.py
+++ voteclimate/management/commands/restart.py
@@ -10,5 +10,5 @@ class Command(BaseCommand):
 		   the application'''
 
 	def handle(self, *args, **options):
-		subprocess.call('/home/voteclimatechange/voteclimatechange.com/restart.sh')
+		subprocess.call(['bash','/home/voteclimatechange/voteclimatechange.com/restart.sh'])
 		self.stdout.write("Ran shell script - passenger set to restart\nThis command does not check that the file was successfully touched, so if you must be sure, check ./tmp/restart.txt")
