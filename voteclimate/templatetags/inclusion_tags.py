333e1fd9944822674b0da9fc791f5e0d58e27fd3
Nick Santos <nickrsan@users.noreply.github.com>
2012-11-01 14:02:42 -0700

    Updates to sidebar, styles, errors, etc. Lots of layout changes

--- voteclimate/templatetags/inclusion_tags.py
+++ voteclimate/templatetags/inclusion_tags.py
@@ -1,6 +1,8 @@
 __author__ = 'Nick'
 
 from django import template, forms
+from django.db.models import Count
+
 from vote_climate import settings
 from voteclimate import models
 
@@ -18,3 +20,8 @@ def global_var(name):
 def facts_box():
 	all_facts = models.fact.objects.all()[:8]
 	return {'facts':all_facts}
+
+@register.inclusion_tag('top_candidates.django_include')
+def top_candidates():
+	candidates = models.candidate.objects.annotate(num_statements = Count('statement')).filter(num_statements__gte=1).order_by('-num_statements')[:15]
+	return {'top_candidates':candidates}
