0c95ed92034d1d7d656e4771e327ab5a09f66380
Nick Santos <nickrsan@users.noreply.github.com>
2012-11-02 00:33:23 -0700

    Fix to setting photo urls

--- voteclimate/management/commands/fix_photo_urls.py
+++ voteclimate/management/commands/fix_photo_urls.py
@@ -3,10 +3,13 @@ __author__ = 'Nick Santos'
 from django.core.management.base import BaseCommand, CommandError
 from voteclimate import utils
 
+import os
+
 class Command(BaseCommand):
 	args = ''
 	help = 'Updates urls for candidate photos'
 
 	def handle(self, *args, **options):
-		utils._fix_photo_urls(r'C:\Users\Nick\workspace\voteclimate')
+		path = os.getcwd()
+		utils._fix_photo_urls(path)
 		self.stdout.write("Done\n")
