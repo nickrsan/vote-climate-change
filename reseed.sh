67eee0fe36b351a7418c181fbae37793a66b446c
Nick Santos <nickrsan@users.noreply.github.com>
2012-11-02 00:03:12 -0700

    addin in script to reseed on server

---
+++ reseed.sh
@@ -0,0 +1,6 @@
+bash ./reset_db.sh
+echo 'Deleted Database'
+python manage.py syncdb --noinput
+echo 'Recreated Database - Adding Data'
+python manage.py seed_db
+python manage.py dumpdata > .\voteclimate\fixtures\dumped.json
